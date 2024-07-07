a7222dd6a34bd11a9ed3b36684f120859f97ab1b
roshan129 <roshanadke2015@gmail.com>
2024-07-07 17:42:08 +0530

    dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation(libs.androidx.glance.appwidget)
+    implementation(libs.androidx.glance.material3)
 }
