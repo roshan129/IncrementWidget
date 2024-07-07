7adafe5f2fe2e2a276f842bfd9b05b6d3c7f553f
roshan129 <roshanadke2015@gmail.com>
2024-07-07 18:56:45 +0530

    Increment widget added.

---
+++ app/src/main/java/com/roshanadke/incrementwidget/ui/IncrementWidget.kt
@@ -0,0 +1,76 @@
+package com.roshanadke.incrementwidget.ui
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.glance.Button
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.ActionParameters
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.background
+import androidx.glance.currentState
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.text.Text
+
+object IncrementWidget : GlanceAppWidget() {
+
+    val countKey = intPreferencesKey("count")
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+
+            val count = currentState(countKey) ?: 0
+            IncrementWidgetUI(count)
+        }
+    }
+}
+
+@Composable
+fun IncrementWidgetUI(count: Int?) {
+
+    Column(
+        modifier = GlanceModifier.fillMaxSize()
+            .background(Color.White),
+        verticalAlignment = Alignment.Vertical.CenterVertically,
+        horizontalAlignment = Alignment.Horizontal.CenterHorizontally
+    ) {
+        Text(text = count.toString(),)
+        Button(text = "Increment", onClick = actionRunCallback(IncrementActionCallback::class.java))
+
+    }
+}
+
+class SimpleIncrementWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget
+        get() = IncrementWidget
+}
+
+class IncrementActionCallback: ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        updateAppWidgetState(context, glanceId) { prefs ->
+            val currentCount = prefs[IncrementWidget.countKey]
+            if(currentCount != null) {
+                prefs[IncrementWidget.countKey] = currentCount + 1
+            } else {
+                prefs[IncrementWidget.countKey] = 1
+            }
+        }
+        IncrementWidget.update(context, glanceId)
+    }
+
+}
+
+
